e500c4ab6001e5bc01a9faaf6c3e59400e553ad6
Denis <denis.chaput77@gmail.com>
2025-03-10 07:09:33 +0100

    Add new texture assets and update texture configuration

--- src/Math/WorldRayCast.cpp
+++ src/Math/WorldRayCast.cpp
@@ -25,11 +25,10 @@ WorldRayCast::WorldRayCast(glm::vec3 position, glm::vec3 direction, World &world
 			glm::vec3 blockPosition = maybeBlockPosition.value();
 			const BlockData *block = world.getBlockAt(blockPosition);
 
-			if (block->type != BlockData::BlockType::air)
+			if (block->type != BlockData::BlockType::air && block->type != BlockData::BlockType::water)
 			{
 				successful = true;
-				std::optional<glm::ivec3> maybeNeighbor =
-					AxisPlane::rayHitsToBlockPosition(prevClosestHits[0], prevClosestHits[1]);
+				std::optional<glm::ivec3> maybeNeighbor = AxisPlane::rayHitsToBlockPosition(prevClosestHits[0], prevClosestHits[1]);
 
 				hitTarget = {blockPosition, *block, maybeNeighbor.value(), hasNeighbor && maybeNeighbor.has_value()};
 			}
